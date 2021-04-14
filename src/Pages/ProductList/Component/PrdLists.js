1046b47883e51a3491f6125fefd6fabf24e9be9f
Eddington524 <iamsam8687@gmail.com>
2021-04-14 17:09:38 +0900

    Modify:productlist 레이아웃 완료 및  조건부 렌더링  수정

--- src/Pages/ProductList/Component/PrdLists.js
+++ src/Pages/ProductList/Component/PrdLists.js
@@ -9,18 +9,20 @@ class PrdLists extends Component {
     // console.log(product);
     return (
       <div className="PrdLists">
-        <li>
-          <img src={product && product.imgSrc} alt="제품 사진" />
-          <div className="conditionBox">
-            <span className="new">new</span>
-            <span className="best">best</span>
-          </div>
-          <div className="prdInfo">
-            <span className="Name">{product && product.title}</span>
-            <span className="hashtag">#{product && product.hashtag}</span>
-            <span className="price">{product && product.price}</span>
-          </div>
-        </li>
+        {product && (
+          <li>
+            <img src={product && product.imgSrc} alt="제품 사진" />
+            <div className="conditionBox">
+              <span className="new">new</span>
+              <span className="best">best</span>
+            </div>
+            <div className="prdInfo">
+              <span className="Name">{product && product.title}</span>
+              <span className="hashtag">#{product && product.hashtag}</span>
+              <span className="price">{product && product.price}</span>
+            </div>
+          </li>
+        )}
       </div>
     );
   }
