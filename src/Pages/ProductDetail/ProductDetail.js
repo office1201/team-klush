36b3dda06525b2992d89a4a1b63c96dda3891622
Eddington524 <iamsam8687@gmail.com>
2021-04-14 19:33:42 +0900

    Add: 디테일 페이지 레이아웃 테스트

---
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ProductDetail.scss';
+import '../../Styles/common.scss';
+
+class ProductDetail extends Component {
+  render() {
+    return;
+    <div></div>;
+  }
+}
+
+export default ProductDetail;
