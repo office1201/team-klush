738bc7da3617e9dec3e162e0dce82a6ac9435154
taehyunKim <office.div1201@gmail.com>
2021-04-16 21:56:07 +0900

    Add: 회원가입 페이지 기능 구현

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -4,10 +4,97 @@ import '../../Styles/reset.scss';
 import '../../Styles/common.scss';
 
 class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    passwordCheck: '',
+    username: '',
+    nickname: '',
+    emailAdress: '',
+    phone: '',
+  };
+  // 1. 함수1: 인풋 값 받아오기
+  handleInput = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  // 2. 함수2: 아이디 검사
+  idValidator = email => {
+    if (email === '') return;
+    if (email !== '' && !email.includes('@')) return 'inputFail';
+    if (email.includes('@')) return 'inputSuccess';
+  };
+  // 3. 함수3: 비밀번호 검사
+  pwValidatorOne = pw => {
+    if (pw === '') return;
+    if (pw.length <= 5) return 'inputFail';
+    if (pw.length > 5) return 'inputSuccess';
+  };
+  // 4. 함수4: 비밀번호 확인 검사
+  pwValidatorTwo = pwd => {
+    if (pwd === '') return;
+    if (pwd.length <= 5) return 'inputFail';
+    if (pwd.length > 5) return 'inputSuccess';
+  };
+  // 5. 함수5: 이름(성함) 검사
+  nameValidator = name => {
+    if (name === '') return;
+    if (name.length <= 1) return 'inputFail';
+    if (name.length > 1) return 'inputSuccess';
+  };
+  // 6. 함수6: 닉네임 검사
+  nicknameValidator = nickname => {
+    if (nickname === '') return;
+    if (nickname.length <= 1) return 'inputFail';
+    if (nickname.length > 1) return 'inputSuccess';
+  };
+  // 7. 함수7: 이메일 검사
+  emailValidator = emailAdress => {
+    if (emailAdress === '') return;
+    if (emailAdress !== '' && !emailAdress.includes('@')) return 'inputFail';
+    if (emailAdress.includes('@')) return 'inputSuccess';
+  };
+  // 8. 함수8: 휴대폰 번호 검사
+  phoneValidator = phone => {
+    if (phone === '') return;
+    if (phone.length <= 10) return 'inputFail';
+    if (phone.length > 10) return 'inputSuccess';
+  };
+  // 9. 함수9: 회원가입 버튼 검사
+  buttonValidator = (
+    email,
+    password,
+    passwordCheck,
+    username,
+    nickname,
+    emailAdress,
+    phone
+  ) => {
+    if (
+      email.includes('@') &&
+      password.length > 5 &&
+      passwordCheck.length > 5 &&
+      username.length > 5 &&
+      nickname.length > 1 &&
+      emailAdress.includes('@') &&
+      phone.length > 10
+    )
+      return 'activeBtn';
+    return '';
+  };
   render() {
+    const {
+      email,
+      password,
+      passwordCheck,
+      username,
+      nickname,
+      emailAdress,
+      phone,
+    } = this.state;
     return (
       <section className="signUp">
-        <div className="signUpTitle">
+        <div className="signInfo">
           <h2>JOIN US</h2>
           <div>
             <span>약관동의</span>
@@ -31,8 +118,18 @@ class SignUp extends Component {
                 <span>아이디</span>
               </div>
               <div className="tableInput">
-                <input type="text" />
-                <span className="idError">사용가능한 아이디입니다</span>
+                {/* 1. 이메일 입력 검사하기*/}
+                <input
+                  onChange={this.handleInput}
+                  type="text"
+                  name="email"
+                  className={this.idValidator(email)}
+                />
+                <span className={`idError ${this.idValidator(email)}`}>
+                  {email.includes('@')
+                    ? '사용 가능한 이메일 입니다.'
+                    : '이메일을 입력해 주세요.'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -41,7 +138,18 @@ class SignUp extends Component {
                 <span>비밀번호</span>
               </div>
               <div className="tableInput">
-                <input type="password" />
+                {/* 2. 비밀번호 입력 검사하기*/}
+                <input
+                  onChange={this.handleInput}
+                  type="password"
+                  name="password"
+                  className={this.pwValidatorOne(password)}
+                />
+                <span className={`idError ${this.pwValidatorOne(password)}`}>
+                  {password.length > 5
+                    ? '사용 가능한 비밀번호 입니다.'
+                    : '비밀번호를 입력해 주세요.(예: 5자리 이상)'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -50,7 +158,20 @@ class SignUp extends Component {
                 <span>비밀번호 확인</span>
               </div>
               <div className="tableInput">
-                <input type="password" />
+                {/* 3. 비밀번호 입력 확인 검사하기 */}
+                <input
+                  onChange={this.handleInput}
+                  type="password"
+                  name="passwordCheck"
+                  className={this.pwValidatorTwo(passwordCheck)}
+                />
+                <span
+                  className={`idError ${this.pwValidatorTwo(passwordCheck)}`}
+                >
+                  {passwordCheck.length > 5
+                    ? '사용 가능한 비밀번호입니다.'
+                    : '비밀번호를 입력해 주세요.(예: 5자리 이상)'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -59,7 +180,18 @@ class SignUp extends Component {
                 <span>이름</span>
               </div>
               <div className="tableInput">
-                <input type="text" />
+                {/* 4. 이름 검사하기*/}
+                <input
+                  onChange={this.handleInput}
+                  type="text"
+                  name="username"
+                  className={this.nameValidator(username)}
+                />
+                <span className={`idError ${this.nameValidator(username)}`}>
+                  {username.length > 1
+                    ? `사용 가능한 이름입니다.`
+                    : '올바른 이름을 작성해 주세요.'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -68,7 +200,18 @@ class SignUp extends Component {
                 <span>닉네임</span>
               </div>
               <div className="tableInput">
-                <input type="text" />
+                {/* 5. 닉네임 검사하기 */}
+                <input
+                  onChange={this.handleInput}
+                  type="text"
+                  name="nickname"
+                  className={this.nicknameValidator(nickname)}
+                />
+                <span className={`idError ${this.nicknameValidator(nickname)}`}>
+                  {nickname.length > 1
+                    ? `사용 가능한 닉네임입니다.`
+                    : '올바른 이름을 작성해 주세요.'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -77,7 +220,18 @@ class SignUp extends Component {
                 <span>이메일</span>
               </div>
               <div className="tableInput">
-                <input type="email" />
+                {/* 6. 이메일 검사히기 */}
+                <input
+                  onChange={this.handleInput}
+                  type="email"
+                  name="emailAdress"
+                  className={this.emailValidator(emailAdress)}
+                />
+                <span className={`idError ${this.emailValidator(emailAdress)}`}>
+                  {emailAdress.includes('@')
+                    ? '사용 가능한 이메일 주소입니다.'
+                    : '이메일 주소를 입력해 주세요.'}
+                </span>
               </div>
             </li>
             <li className="table">
@@ -85,13 +239,37 @@ class SignUp extends Component {
                 <span className="tableIcon">■</span>
                 <span>휴대폰번호</span>
               </div>
+              {/* 7. 휴대폰 검사하기 */}
               <div className="tableInput">
-                <input type="text" placeholder="- 없이 입력하세요." />
+                <input
+                  onChange={this.handleInput}
+                  type="text"
+                  placeholder="- 없이 입력하세요."
+                  name="phone"
+                  className={this.phoneValidator(phone)}
+                />
+                <span className={`idError ${this.phoneValidator(phone)}`}>
+                  {phone.length > 10
+                    ? '사용 가능한 휴대폰 번호입니다.'
+                    : '올바른 이름을 작성해 주세요.'}
+                </span>
               </div>
             </li>
           </ul>
-          <li className="signUpBtn">
-            <button className="btn">회원가입</button>
+          <li className="signBtn">
+            <button
+              className={`btn ${this.buttonValidator(
+                email,
+                password,
+                passwordCheck,
+                username,
+                nickname,
+                emailAdress,
+                phone
+              )}`}
+            >
+              회원가입
+            </button>
           </li>
         </form>
       </section>
