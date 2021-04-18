b0a2f6b375bd8cd65eba4cb0f0dea9faefd92b52
taehyunKim <office.div1201@gmail.com>
2021-04-18 17:39:59 +0900

    Add: 댓글 컴포넌트 레이아웃

---
+++ src/Pages/ProductDetail/Components/Comments/Comments.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import MemberNon from '../MemberNon/MemberNon';
+import Member from './Member/Member';
+
+class Comments extends Component {
+  render() {
+    return (
+      <>
+        <Member />
+        {/* <MemberNon /> */}
+      </>
+    );
+  }
+}
+
+export default Comments;
