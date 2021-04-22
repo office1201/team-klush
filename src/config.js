c63fcac8525a7758d5e97e653441232bcbbc54c4
Kim-Heeyeol <76519867+Kim-Heeyeol@users.noreply.github.com>
2021-04-22 21:54:17 +0900

    Add: 충돌 해결

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export const PRODUCTS_API =
 
 export const LOGIN_API = 'http://13.125.224.154:8000/users/signin';
 export const CART_API = 'http://13.125.224.154:8000/orders/cart';
-export const MAIN_API = 'http://13.125.224.154:8000/products/menu-bar';
+export const MENU_API = 'http://13.125.224.154:8000/products/menu-bar';
 export const PRDDETAIL_API = 'http://13.125.224.154:8000/products';
 export const SIGNUP_API = 'http://10.58.2.24:8000/users/signup';
+export const MAIN_API = 'http://13.125.224.154:8000/products/main';
