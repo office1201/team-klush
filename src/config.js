9d65f9c803bf7cff51280883656f3a68501011f9
Eddington524 <iamsam8687@gmail.com>
2021-04-23 18:13:14 +0900

    config.js 서버주소 변경 완료 진짜 최최종git add .

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export const PRODUCTS_API =
-  'http://13.125.208.88:8000/products/category?main_category_id=4';
-export const COMMENT_API = 'http://13.125.208.88:8000/ratings/comment';
-export const STAR_API = 'http://13.125.208.88:8000/ratings?user=1&product=1';
-export const LOGIN_API = 'http://13.125.208.88:8000/users/signin';
-export const CART_API = 'http://13.125.208.88:8000/orders/cart';
-export const MENU_API = 'http://13.125.208.88:8000/products/menu-bar';
-export const PRDDETAIL_API = 'http://13.125.208.88:8000/products';
-export const SIGNUP_API = 'http://13.125.208.88:8000/users/signup';
-export const MAIN_API = 'http://13.125.208.88:8000/products/main';
+  'http://13.125.224.154:8000/products/category?main_category_id=4';
+export const COMMENT_API = 'http://13.125.224.154:8000/ratings/comment';
+export const STAR_API = 'http://13.125.224.154:8000/ratings?user=1&product=1';
+export const LOGIN_API = 'http://13.125.224.154:8000/users/signin';
+export const CART_API = 'http://13.125.224.154:8000/orders/cart';
+export const MENU_API = 'http://13.125.224.154:8000/products/menu-bar';
+export const PRDDETAIL_API = 'http://13.125.224.154:8000/products';
+export const SIGNUP_API = 'http://13.125.224.154:8000/users/signup';
+export const MAIN_API = 'http://13.125.224.154:8000/products/main';
